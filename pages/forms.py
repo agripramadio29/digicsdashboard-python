a74dafa571410fec60c20b2a7102270422bfe6b9
agripramadio29 <119289271+agripramadio29@users.noreply.github.com>
2023-05-18 19:39:27 +0700

    added forms

--- pages/forms.py
+++ pages/forms.py
@@ -6,6 +6,18 @@ from django.contrib.auth.models import User
 
 # user form
 class CreateUserForm(UserCreationForm):
+
+    email = forms.EmailField(max_length=50, required=True)
+    # password1 = forms.CharField(max_length=50, widget=forms.PasswordInput, required=True)
+    # password2 = forms.CharField(max_length=50, widget=forms.PasswordInput, required=True)
+
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+    
+    def save(self, commit=True):
+        user = super(CreateUserForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
