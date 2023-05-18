7a8055b804dc5f5ceab5cd5f358664ac2a477155
agripramadio29 <119289271+agripramadio29@users.noreply.github.com>
2023-05-18 22:33:05 +0700

    add user test

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from pages.views import home, dashboard, user
+from django.contrib.auth.models import User
+
+import pytest
 
 # Create your tests here.
+class testCases(SimpleTestCase):
+
+    # tes url
+    def testHomeUrl(self):
+        url = reverse('home')
+        self.assertEqual(resolve(url).func, home)
+    def testDashboardUrl(self):
+        url = reverse('dashboard')
+        self.assertEqual(resolve(url).func, dashboard)
+    def testUserUrl(self):
+        url = reverse('user')
+        self.assertEqual(resolve(url).func, user)
+
+
+    # test user outside testclass
+@pytest.mark.django_db
+def testCreateUser():
+    User.objects.create_user('agri2', 'afryanto@email.com', '123456')
+    assert User.objects.count() == 1
+
+
+
+    
